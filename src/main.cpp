41c71cb1c5461e2663486960bc3e9da3e037cf30
Sarthak Khandare <sarthakkhandare21@gmail.com>
2023-08-09 09:59:43 +0530

    working code soil moisture

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,30 @@
 #include <ESP8266WiFi.h>
 #endif
 #include <Firebase_ESP_Client.h>
-
+#include <Arduino.h>
+// Provide the token generation process info.
 #include <addons/TokenHelper.h>
 
+// Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
 
+/* 1. Define the WiFi credentials */
 #define WIFI_SSID "RNXG"
 #define WIFI_PASSWORD "Loop@rnxg22-23"
 
+// For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
+
+/* 2. Define the API Key */
 #define API_KEY "AIzaSyDEQbyt0lAQSAIA75lgoSwAqJvoA52GDYA"
 
-#define DATABASE_URL "https://myesp32-615e9-default-rtdb.firebaseio.com/"
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "https://myesp32-615e9-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "rnxgattendance2022@gmail.com"
 #define USER_PASSWORD "rnxg@2022"
 
+// Define Firebase Data object
 FirebaseData fbdo;
 
 FirebaseAuth auth;
@@ -27,11 +36,13 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 
 unsigned long count = 0;
+int _moisture, sensor_analog;
+const int sensor_pin = 32;
 
 void setup()
 {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -46,24 +57,51 @@ void setup()
   Serial.println();
 
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
   config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
+  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
-  config.token_status_callback = tokenStatusCallback;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Or use legacy authenticate method
   config.database_url = DATABASE_URL;
   config.signer.tokens.legacy_token = "<AE7GafzXdp0TOH4cFp3ts5lPrIZdbT9tIpq4VmXq>";
+
   Firebase.begin(&config, &auth);
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library
   Firebase.reconnectWiFi(true);
 }
 
 void loop()
 {
+  // Flash string (PROGMEM and  (FPSTR), String C/C++ string, const char, char array, string literal are supported
+  // in all Firebase and FirebaseJson functions, unless F() macro is not supported.
+
+  // Firebase.ready() should be called repeatedly to handle authentication tasks.
+
   if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
   {
-    sendDataPrevMillis = millis();
-
+    // Read soil moisture value from the sensor
+    sensor_analog = analogRead(sensor_pin);
+    _moisture = (100 - ((sensor_analog / 4095.00) * 100));
     FirebaseJson json;
     json.setDoubleDigits(3);
+    json.set("moisture", _moisture); // Add the moisture value to the JSON
+    // Your existing Firebase communication code...
+    // Push the JSON data to the Firebase database
+    Serial.printf("Push json... %s\n", Firebase.RTDB.pushJSON(&fbdo, "/test/push", &json) ? "ok" : fbdo.errorReason().c_str());
+    json.set("moisture", _moisture);
+    delay(5000);
+    // Update the node with the updated JSON data
+    Serial.printf("Update json... %s\n\n", Firebase.RTDB.updateNode(&fbdo, "/test/push/" + fbdo.pushName(), &json) ? "ok" : fbdo.errorReason().c_str());
+    count++;
   }
 }
